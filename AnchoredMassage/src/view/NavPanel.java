ba56e408cd0a1c9f3201f72d1b891932089c171f
nicholas hays <nicholas.hays@hotmail.com>
2016-05-04 20:47:53 -0700

    NavPanel component

---
+++ AnchoredMassage/src/view/NavPanel.java
@@ -0,0 +1,17 @@
+package view;
+
+
+import javax.swing.JPanel;
+
+public class NavPanel extends JPanel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6753056291905526403L;
+
+	
+	public NavPanel() {
+		
+	}
+}
