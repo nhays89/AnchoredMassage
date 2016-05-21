86118848606c531e10c607ae7310243951fd6e46
nicholas hays <nicholas.hays@hotmail.com>
2016-05-21 01:08:58 -0700

    added propertychangelisteners

--- AnchoredMassage/src/view/AppointmentCard.java
+++ AnchoredMassage/src/view/AppointmentCard.java
@@ -3,6 +3,11 @@ package view;
 import java.awt.BorderLayout;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javax.swing.JPanel;
 import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 import view.AnchoredGUI;
@@ -31,12 +36,17 @@ public class AppointmentCard extends JPanel {
 	 * Table Panel.
 	 */
 	AppointmentTablePanel myAppointmentTablePanel;
+	/**
+	 * Patient Meta Data
+	 */
+	protected static ResultSetMetaData APPOINTMENT_META_DATA;
 
 	/**
 	 * Creates the Appointment Card components.
 	 */
 	public AppointmentCard() {
 		setLayout(new BorderLayout());
+		setApptMetaData();
 		myAppointmentUpdatePanel = new AppointmentUpdatePanel();
 		myAppointmentSearchPanel = new AppointmentSearchPanel();
 		myAppointmentTablePanel = new AppointmentTablePanel();
@@ -44,8 +54,20 @@ public class AppointmentCard extends JPanel {
 		add(myAppointmentSearchPanel, BorderLayout.NORTH);
 		add(myAppointmentTablePanel, BorderLayout.CENTER);
 		myAppointmentTablePanel.addPropertyChangeListener(myAppointmentUpdatePanel);
+		myAppointmentTablePanel.addPropertyChangeListener(myAppointmentSearchPanel);
 		myAppointmentUpdatePanel.addPropertyChangeListener(myAppointmentTablePanel);
+		myAppointmentSearchPanel.addPropertyChangeListener(myAppointmentUpdatePanel);
 		myAppointmentSearchPanel.addPropertyChangeListener(myAppointmentTablePanel);
+	}
+
+	private void setApptMetaData() {
+		try {
+			Statement stmt = AnchoredGUI.DB_CONNECTION.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM APPOINTMENT");
+			APPOINTMENT_META_DATA = rs.getMetaData();
+		} catch (SQLException e) {
+			new MSGWindow(e.getMessage());
+		}
 	}
 
 }
